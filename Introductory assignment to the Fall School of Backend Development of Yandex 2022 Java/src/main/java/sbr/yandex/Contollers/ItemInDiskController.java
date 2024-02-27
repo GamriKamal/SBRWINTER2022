db0c1562738456c07cb147947785d09b906486b6
GamriKamal <124342926+GamriKamal@users.noreply.github.com>
2024-02-27 20:33:57 +0600

    Update ItemInDiskController.java
    Сhanged the return data type of the methods. I made the code more structured, now the code outputs http codes

--- Introductory assignment to the Fall School of Backend Development of Yandex 2022 Java/src/main/java/sbr/yandex/Contollers/ItemInDiskController.java
+++ Introductory assignment to the Fall School of Backend Development of Yandex 2022 Java/src/main/java/sbr/yandex/Contollers/ItemInDiskController.java
@@ -3,25 +3,13 @@ package sbr.yandex.Contollers;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import sbr.yandex.Entities.ItemInDisk;
 import sbr.yandex.Services.ItemInDiskService;
 import sbr.yandex.Services.Parsing;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.TemporalAccessor;
-import java.util.regex.Pattern;
 
 @Controller
 public class ItemInDiskController {
@@ -29,47 +17,68 @@ public class ItemInDiskController {
     @Autowired private ItemInDiskService service;
 
     @PostMapping("/imports")
-    public void importElements(@RequestBody String result) throws JSONException {
-        parsing.parseJSONArray(result);
+    public ResponseEntity<String> importElements(@RequestBody String result) throws JSONException {
+        return parsing.parseJSONArray(result);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteElements(@PathVariable String id){
-        service.deleteFolderOrFileById(id);
+    public ResponseEntity<String> deleteElements(@PathVariable String id){
+        return service.deleteFolderOrFileById(id);
     }
 
     @GetMapping("/nodes/{id}")
     public ResponseEntity<String> getInfoAboutElementById(@PathVariable String id) throws JSONException {
-        JSONObject responseObject = service.getInfoOfElementById(id);
-        String jsonString = responseObject.toString();
+        String jsonString = service.getInfoOfElementById(id).toString();
 
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(jsonString);
+        if(!jsonString.isEmpty()){
+            return ResponseEntity.status(200)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(jsonString);
+        } else {
+            return ResponseEntity.status(404).body("There is no entity by this id");
+        }
     }
 
     @GetMapping("/updates")
     public ResponseEntity<String> getUpdateList(@RequestParam("date") String date) throws JSONException {
-        if (!isValidISO8601(date)) {
-            return ResponseEntity.badRequest().body("Invalid date format. Please use ISO 8601 format.");
+        if (!ItemInDisk.isValidISO8601(date)) {
+            return ResponseEntity.status(400).body("Invalid date format. Please use ISO 8601 format.");
         }
+        String jsonString = service.getUpdateListByDate(date).toString();
 
-        JSONObject responseObject = service.getUpdateListByDate(date);
+        if(!jsonString.isEmpty()){
+            return ResponseEntity.status(200)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(jsonString);
+        } else {
+            return ResponseEntity.status(404).body("There is no entity by this id");
+        }
+    }
+
+    @GetMapping("/node/{id}/history")
+    public ResponseEntity<String> getHistoryOfUpdates(@PathVariable String id,
+                                                      @RequestParam("dateStart") String dateStart,
+                                                      @RequestParam("dateEnd") String dateEnd) throws JSONException {
+        JSONObject responseObject = service.getHistoryOfUpdatesByIdAndDates(id, ItemInDisk.convertStringToTime(dateStart), ItemInDisk.convertStringToTime(dateEnd));
         String jsonString = responseObject.toString();
 
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(jsonString);
-    }
-
-    private static boolean isValidISO8601(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-        try {
-            LocalDateTime parsedDatetime = LocalDateTime.parse(date, formatter);
-            return true;
-        } catch (DateTimeParseException e) {
-            return false;
+        if(!jsonString.isEmpty()){
+            return ResponseEntity.status(200)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(jsonString);
+        } else {
+            return ResponseEntity.status(404).body("There is no entity by this id");
         }
     }
+
+//    private ResponseEntity<String> getHTTPCode(String jsonString){
+//        if(!jsonString.isEmpty()){
+//            return ResponseEntity.status(200)
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .body(jsonString);
+//        } else {
+//            return ResponseEntity.status(404).body("There is no entity by this id");
+//        }
+//    }
 
 }
